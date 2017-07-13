925311a1d9d41191bc19db78f0387b5c64174c51
pstiebel <pstiebel@MSEDGEWIN10>
2017-07-13 11:47:06 +0200

    Initial Eclipse commit

---
+++ MyMavenTestProject/src/main/java/pstiebel_01/test/App.java
@@ -0,0 +1,13 @@
+package pstiebel_01.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
